b0368c0484fed8d38d912408bbed3762e185a7cb
Bowen Xie <bowenxie2006@hotmail.com>
2014-09-19 20:39:36 -0500

    Configuration for ASSN0

--- defs.mk
+++ defs.mk
@@ -13,7 +13,7 @@
 # The things that can be set here are documented in mk/os161.config.mk.
 #
 
-OSTREE=/u2/bwxie/cs350-os161/root
+OSTREE=/home/bowen/cs350-os161/root
 PLATFORM=sys161
 MACHINE=mips
 COMPAT_CFLAGS=
